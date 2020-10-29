d575027736e74f33035e194d954f09072c4a2f39
Kha Đỗ Văn <dovankha.tlvn@outlook.com>
2020-10-29 16:22:57 +0700

    Kha update - Phan giao dien menu

--- dic1.0.cpp
+++ dic1.0.cpp
@@ -1,22 +1,28 @@
-#include <iostream> 
-using namespace std;
-#include <fstream> 
-#include<string>
-#include<conio.h>
-#include<stdio.h>
-#include<Windows.h> 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <stdio.h>
+#include <Windows.h>
 #include <iomanip>
 
+using namespace std;
+
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
 class dictionary
 {
 	int Id;
 	string Tu;
+
 public:
 	dictionary();
 	dictionary(int id, string tu);
 	int getId();
-	string getTu() { return Tu; }
+	string getTu()
+	{
+		return Tu;
+	}
 	void setId(int new_id)
 	{
 		Id = new_id;
@@ -27,7 +33,7 @@ public:
 	}
 	void printTu()
 	{
-		cout<<setw(5)<<left<<Tu<<"\t";
+		cout << setw(5) << left << Tu << "\t";
 	}
 };
 
@@ -48,31 +54,39 @@ int dictionary::getId()
 	return Id;
 }
 
-
-class T_M_E :public dictionary
+// T_M_E : type-mean-example
+class T_M_E : public dictionary
 {
 	string loaiTu;
 	string Nghia;
 	string viDu;
+
 public:
-	T_M_E() {
+	T_M_E()
+	{
 		loaiTu = "";
 		Nghia = "";
 		viDu = "";
 	}
 	T_M_E(string, string, string);
 	T_M_E(int, string, string, string, string);
-	void xuatfullTu() {
+	void xuatfullTu()
+	{
 		// cout << "Type: "<< loaiTu << endl << "Mean: "<< Nghia << endl << "Example: "<< viDu << endl;
 		SetConsoleTextAttribute(console, 116);
-		cout << setw(5)<<left<<"Word"<<"\t"<<setw(20) << left << "Type" << "\t" <<setw(40) << left << "Mean" <<"\t"<<setw(40) << left << "Example" <<"\t"<<endl;
+		cout << setw(5) << left << "Word"
+			 << "\t" << setw(20) << left << "Type"
+			 << "\t" << setw(40) << left << "Mean"
+			 << "\t" << setw(40) << left << "Example"
+			 << "\t" << endl;
 		SetConsoleTextAttribute(console, 7);
 		this->printTu();
-		cout <<setw(20) << left << loaiTu <<"\t";
-		cout <<setw(40) << left << Nghia <<"\t";
-		cout <<setw(40) << left << viDu <<"\t";
+		cout << setw(20) << left << loaiTu << "\t";
+		cout << setw(40) << left << Nghia << "\t";
+		cout << setw(40) << left << viDu << "\t";
 	}
-	void setfullTu(T_M_E p) {
+	void setfullTu(T_M_E p)
+	{
 		loaiTu = p.loaiTu;
 		Nghia = p.Nghia;
 		viDu = p.viDu;
@@ -81,7 +95,7 @@ public:
 	}
 };
 
-T_M_E::T_M_E(int id, string tu, string Ty, string Me, string Ex) :dictionary(id, tu)
+T_M_E::T_M_E(int id, string tu, string Ty, string Me, string Ex) : dictionary(id, tu)
 {
 	this->loaiTu = Ty;
 	this->Nghia = Me;
@@ -91,28 +105,32 @@ T_M_E::T_M_E(int id, string tu, string Ty, string Me, string Ex) :dictionary(id,
 struct node
 {
 	T_M_E data;
-	node* pNext;
+	node *pNext;
 };
-typedef struct node NODE; //thay the struct node thành NODE
+typedef struct node NODE; //thay the struct node thï¿½nh NODE
+
 struct list
 {
-	NODE* pHead;
+	NODE *pHead;
 };
 typedef struct list LIST;
 
-void KhoiTao(LIST& l)
+void KhoiTao(LIST &l)
 {
 	l.pHead = NULL;
 }
 
-bool isEmpty(LIST& l) {
-	if (l.pHead == NULL) return true;
+bool isEmpty(LIST &l)
+{
+	if (l.pHead == NULL)
+		return true;
 
 	return false;
 }
-NODE* KhoiTaoNODE(const T_M_E& x)
+
+NODE *KhoiTaoNODE(const T_M_E &x)
 {
-	NODE* p = new NODE;
+	NODE *p = new NODE;
 	if (p == NULL)
 	{
 		cout << "Khong du bo nho cap phat !";
@@ -124,7 +142,7 @@ NODE* KhoiTaoNODE(const T_M_E& x)
 	return p;
 }
 
-void themVaoDau(LIST& l, NODE* p)
+void themVaoDau(LIST &l, NODE *p)
 {
 	if (l.pHead == NULL)
 	{
@@ -137,17 +155,20 @@ void themVaoDau(LIST& l, NODE* p)
 	}
 }
 
-NODE* findTail(LIST& l) {
-	if (isEmpty(l)) return NULL;
-	NODE* p = l.pHead;
-	while (p->pNext != NULL) {
+NODE *findTail(LIST &l)
+{
+	if (isEmpty(l))
+		return NULL;
+	NODE *p = l.pHead;
+	while (p->pNext != NULL)
+	{
 		p = p->pNext;
 	}
 
 	return p;
 }
 
-void themVaoCuoi(LIST& l, NODE* p)
+void themVaoCuoi(LIST &l, NODE *p)
 {
 	if (l.pHead == NULL)
 	{
@@ -161,16 +182,21 @@ void themVaoCuoi(LIST& l, NODE* p)
 
 void xuatDanhSach(LIST l)
 {
-	if (l.pHead == NULL) return;
-	for (NODE* k = l.pHead; k != NULL; k = k->pNext) {
+	if (l.pHead == NULL)
+		return;
+	for (NODE *k = l.pHead; k != NULL; k = k->pNext)
+	{
 		k->data.xuatfullTu();
 		cout << endl;
 	}
 }
+
 int main()
 {
 	SetConsoleTitle(TEXT("Tu Dien Anh-Viet"));
-	LIST l; KhoiTao(l);
+
+	LIST l;
+	KhoiTao(l);
 	ifstream fw, ft, fm, fe;
 	string tu, loai, nghia, vd;
 	int count = 0;
@@ -182,56 +208,68 @@ int main()
 	{
 		count++;
 		getline(fw, tu, ' ');
-		getline(ft, loai, ' '); 
+		getline(ft, loai, ' ');
 		getline(fm, nghia, '@');
 		getline(fe, vd, '@');
 		T_M_E tuVung(count, tu, loai, nghia, vd);
-		NODE* p = KhoiTaoNODE(tuVung);
+		NODE *p = KhoiTaoNODE(tuVung);
 		themVaoCuoi(l, p);
 	}
 	/*cout << "Danh sach lien ket don co:" << endl;
 	xuatDanhSach(l);*/
-	int luaChon; 
-	while (true) 
+	int luaChon;
+	while (true)
 	{
-		system("cls"); 
-		cout<<"Tu Dien Anh - Viet"<<endl<<"Cac chuc nang: "<<endl
-			<<"1. Tim kiem."<<endl
-			<<"2. Them tu."<<endl
-			<<"3. Chinh sua tu."<<endl
-			<<"4. Xoa tu."<<endl
-			<<"5. Thoat khoi tu dien."<<endl;
-		cout<<"Nhap lua chon: "; 
-		cin>>luaChon; 
-		if (luaChon==1) 
+		system("cls");
+		SetConsoleTextAttribute(console, 91
+		);
+		cout << "\t\t\t\t||==============================||" << endl
+			 << "\t\t\t\t||         ****MENU****         ||" << endl
+			 << "\t\t\t\t||   1. Tim kiem                ||" << endl
+			 << "\t\t\t\t||   2. Them tu                 ||" << endl
+			 << "\t\t\t\t||   3. Chinh sua tu            ||" << endl
+			 << "\t\t\t\t||   4. Xoa tu                  ||" << endl
+			 << "\t\t\t\t||   0. Thoat khoi tu dien      ||" << endl
+			 << "\t\t\t\t||==============================||"<<endl;
+		SetConsoleTextAttribute(console, 7);
+		cout << "\n\n\tMoi ban nhap lua chon: ";
+		cin >> luaChon;
+		if (luaChon < 0 || luaChon > 4)
+		{
+			cout << "\n\tLua chon cua ban khong dung!";
+			getch();
+		}
+		
+		else if (luaChon == 1)
 		{
 			system("cls");
-			bool timKiem=false; 
+			bool timKiem = false;
 			string search;
-			cout << "Nhap tu can tim kiem: ";
+			cout << "\n\t\tNhap tu can tim kiem: ";
 			cin >> search;
-			for (NODE* k = l.pHead; k != NULL; k = k->pNext) 
+			cout << "\t\t----------------------\n" << endl;
+			for (NODE *k = l.pHead; k != NULL; k = k->pNext)
 				if (search == k->data.getTu())
 				{
-					timKiem=true; 
+					timKiem = true;
 					k->data.xuatfullTu();
-					cout<<endl;
+					cout << endl;
 					getch();
 				}
-			if (!timKiem) 
+			if (!timKiem)
 			{
-				cout<<"Tu '"<<search<<"' chua co trong tu dien!"<<endl
-					<<"Nhan Enter de quay lai menu chuc nang" << endl;
+				cout << "Tu '" << search << "' chua co trong tu dien!" << endl
+					 << "Nhan phim bat ki de quay lai!" << endl;
 				getch();
 			}
 		}
-		else if (luaChon == 5)
+		else // luaChon == 0;
 			break;
 	}
+
 	fw.close();
 	ft.close();
 	fm.close();
 	fe.close();
 	return 0;
 }
-
